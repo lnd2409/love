0512c5eea9eb75277e6d0191e90efa65d4061287
Ngoc Duc <lnd240998@gmail.com>
2021-12-29 00:09:49 +0700

    update date

--- main.js
+++ main.js
@@ -11,7 +11,7 @@
         mm = String(today.getMonth() + 1).padStart(2, "0"),
         yyyy = today.getFullYear(),
         nextYear = yyyy + 1,
-        dayMonth = "01/25/",
+        dayMonth = "01/20/",
         birthday = dayMonth + 2022;
 
     today = mm + "/" + dd + "/" + yyyy;
