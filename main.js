c23b45a845bd865c05bcd138ef125882a988233b
Ngoc Duc <lnd240998@gmail.com>
2021-12-30 00:47:04 +0700

    push code

--- main.js
+++ main.js
@@ -11,7 +11,7 @@
         mm = String(today.getMonth() + 1).padStart(2, "0"),
         yyyy = today.getFullYear(),
         nextYear = yyyy + 1,
-        dayMonth = "01/20/",
+        dayMonth = "01/15/",
         birthday = dayMonth + 2022;
 
     today = mm + "/" + dd + "/" + yyyy;
