6029776d7a5dedd5fd4eb559623bcda3971c14fe
Duc Le <lnd240998@gmail.com>
2023-09-30 07:54:44 +0700

    add

--- camtasia/scripts/config_xml.js
+++ camtasia/scripts/config_xml.js
@@ -2,8 +2,8 @@ var TSC = TSC || {};
 
 TSC.embedded_config_xml = '<x:xmpmeta xmlns:x="adobe:ns:meta/">\
    <rdf:RDF xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:xmp="http://ns.adobe.com/xap/1.0/" xmlns:xmpDM="http://ns.adobe.com/xmp/1.0/DynamicMedia/" xmlns:xmpG="http://ns.adobe.com/xap/1.0/g/" xmlns:tsc="http://www.techsmith.com/xmp/tsc/" xmlns:xmpMM="http://ns.adobe.com/xap/1.0/mm/" xmlns:tscDM="http://www.techsmith.com/xmp/tscDM/" xmlns:tscIQ="http://www.techsmith.com/xmp/tscIQ/" xmlns:tscHS="http://www.techsmith.com/xmp/tscHS/" xmlns:stDim="http://ns.adobe.com/xap/1.0/sType/Dimensions#" xmlns:stFnt="http://ns.adobe.com/xap/1.0/sType/Font#" xmlns:exif="http://ns.adobe.com/exif/1.0" xmlns:dc="http://purl.org/dc/elements/1.1/">\
-      <rdf:Description tsc:version="2.0.1" dc:date="2023-09-30 06:35:34 AM" dc:source="Camtasia Studio,8.4.0,enu" dc:title="hoat-dong-cua-be" tscDM:firstFrame="hoat-dong-cua-be_First_Frame.png" tscDM:originId="8B0863EA-47E6-4DA1-8B58-BB52BB49972B" tscDM:project="Untitled">\
-         <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="768500"/>\
+      <rdf:Description tsc:version="2.0.1" dc:date="2023-09-30 07:53:27 AM" dc:source="Camtasia Studio,8.4.0,enu" dc:title="hoat-dong-cua-be" tscDM:firstFrame="hoat-dong-cua-be_First_Frame.png" tscDM:originId="8B0863EA-47E6-4DA1-8B58-BB52BB49972B" tscDM:project="Untitled">\
+         <xmpDM:duration xmpDM:scale="1/1000" xmpDM:value="794533"/>\
          <xmpDM:videoFrameSize stDim:unit="pixel" stDim:h="720" stDim:w="1280"/>\
          <tsc:langName>\
             <rdf:Bag>\
@@ -15,7 +15,7 @@ TSC.embedded_config_xml = '<x:xmpmeta xmlns:x="adobe:ns:meta/">\
                   <rdf:Description xmpDM:trackType="Hotspot" xmpDM:frameRate="f1000" xmpDM:trackName="Hotspots">\
                      <xmpDM:markers>\
                         <rdf:Seq>\
-                           <rdf:li><rdf:Description xmp:label="1" xmpDM:startTime="751370" xmpDM:duration="13300" tscDM:boundingPoly="159,269;1120,269;1120,449;159,449;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li></rdf:Seq>\
+                           <rdf:li><rdf:Description xmp:label="1" xmpDM:startTime="781200" xmpDM:duration="13300" tscDM:boundingPoly="159,269;1120,269;1120,449;159,449;" tscDM:rotate="0.000000" tscHS:pause="1"/></rdf:li></rdf:Seq>\
                      </xmpDM:markers>\
                   </rdf:Description>\
                </rdf:li>\
